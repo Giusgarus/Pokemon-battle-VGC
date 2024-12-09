1294deeaf46910200b359509df87ee08fd7179b7
Giuseppe Gabriele Russo <khrusso99@hotmail.com>
2024-12-09 21:21:06 +0100

    Update MinMaxBattlePolicies.py

--- Agents/MinMax/MinMaxBattlePolicies.py
+++ Agents/MinMax/MinMaxBattlePolicies.py
@@ -9,20 +9,21 @@ from vgc.datatypes.Constants import DEFAULT_PKM_N_MOVES, DEFAULT_PARTY_SIZE, TYP
 from vgc.datatypes.Objects import GameState, PkmTeam, PkmMove
 from vgc.datatypes.Types import PkmStat, PkmType, WeatherCondition
 
-class Node:
-    def __init__(self, env: PkmBattleEnv, parent: Node = None, actions: list[int] = None):
-        self.parent: Node = parent
+class MMNode:
+    def __init__(self, env: PkmBattleEnv, parent: 'MMNode' = None, actions: list[int] = None):
+        self.parent: MMNode = parent
         self.actions = actions
-        self.children: list[Node] = []
+        self.children: list[MMNode] = []
         self.env: PkmBattleEnv = env
         self.is_leaf = True
+
     
     def get_actions(self,current_team) -> list[int]:
         team = self.env.teams[current_team]
         actions_pkm = team.active.moves
         actions_list = []
         for i in range(len(actions_pkm)):
-                actions_list.append([actions_pkm[i].move_id])
+            actions_list.append([actions_pkm[i].move_id])
         return actions_list
 
 
@@ -63,24 +64,32 @@ class MinMaxBattlePolicy:
         return max_eval
         '''
 
-    def evaluate_game_state(self, game_state: GameState) -> float:
+    def evaluate_node(self, game_state: GameState) -> float:
         my_team = game_state.teams[0]
         opp_team = game_state.teams[1]
 
         my_score = 0
         opp_score = 0
 
+        # Considera le win condition
+        if(game_state.is_winner()):
+            my_score += 100000
+        else:
+            opp_score += 100000
+
+
         # Considera la salute residua dei Pokémon
         for pkm in my_team.get_pkm_list():
-            my_score += pkm.hp / pkm.max_hp
+            my_score += 5*(pkm.hp / pkm.max_hp)
         for pkm in opp_team.get_pkm_list():
-            opp_score += pkm.hp / pkm.max_hp
+            opp_score += 5*(pkm.hp / pkm.max_hp)
 
         # Considera il numero di Pokémon rimasti
-        my_score += len([pkm for pkm in my_team.pkm_list if pkm.current_hp > 0])
-        opp_score += len([pkm for pkm in opp_team.pkm_list if pkm.current_hp > 0])
+        my_score += 500*len([pkm for pkm in my_team.pkm_list if pkm.current_hp > 0])
+        opp_score += 500*len([pkm for pkm in opp_team.pkm_list if pkm.current_hp > 0])
 
         # Considera le condizioni di stato
+        '''Qui devo differenziare meglio perché sonno e bruciatura non valgono uguali ad esempio'''
         for pkm in my_team.get_pkm_list():
             if pkm.status != 0:
                 my_score -= 0.5
@@ -92,14 +101,29 @@ class MinMaxBattlePolicy:
         my_active_pkm = my_team.active
         opp_active_pkm = opp_team.active
         if my_active_pkm and opp_active_pkm:
-            my_score += TYPE_CHART_MULTIPLIER[my_active_pkm.type][opp_active_pkm.type]
-            opp_score += TYPE_CHART_MULTIPLIER[opp_active_pkm.type][my_active_pkm.type]
+            if TYPE_CHART_MULTIPLIER[my_active_pkm.type][opp_active_pkm.type] ==2.:
+                my_score += 200
+            elif TYPE_CHART_MULTIPLIER[my_active_pkm.type][opp_active_pkm.type] == .5:
+                my_score -= 100
 
-        # Considera le condizioni meteorologiche
-        if game_state.weather_condition == WeatherCondition.SUNNY:
-            my_score += 0.1
-        elif game_state.weather_condition == WeatherCondition.RAINY:
-            opp_score += 0.1
+            if TYPE_CHART_MULTIPLIER[opp_active_pkm.type][my_active_pkm.type] ==2.:
+                opp_score += 200
+            elif TYPE_CHART_MULTIPLIER[opp_active_pkm.type][my_active_pkm.type] == .5:
+                opp_score -= 100
+
+        # Considera il vantaggio delle condizioni meteorologiche
+        '''
+        if stato favorevole a pokemon attivo:
+            my_score += 150
+        elif stato favorevole a pokemon attivo avversario:
+            opp_score += 150
+        '''
+
+        #Win or lose condition
+        if game_state.is_winner():
+            my_score += 100000
+        else:
+            opp_score += 10000
 
         return my_score - opp_score
     
@@ -119,10 +143,16 @@ class MinMaxBattlePolicy:
     
 
 class MinMaxPlayer(BattlePolicy):
-    def __init__(self, depth):
-        super().__init__()
-        self.depth = depth
-        self.minmax = MinMaxBattlePolicy(depth)
 
-    def choose_move(self, game_state: GameState) -> PkmMove:
-        return self.minmax.minmax(game_state)
+    def __init__(self, player_index: int, enable_print=False):
+        super().__init__()
+        self.name = f'Player {player_index}'
+        self.player_index = player_index
+    
+    def __str__(self):
+        print(self.name)
+
+    def get_action(self, state: GameState) -> int:
+        root = MMNode(state.env)
+        best_move = self.minimax(root)
+        return best_move
