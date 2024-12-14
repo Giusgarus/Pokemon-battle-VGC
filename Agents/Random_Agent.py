f3690a6229edbd173706e0aa017b8bd85a1b1219
Gianluca <gianluca.panzani97@gmail.com>
2024-12-14 12:39:08 +0100

    Random agent with index

--- Agents/Random_Agent.py
+++ Agents/Random_Agent.py
@@ -1,26 +1,30 @@
+import numpy as np
 from vgc.behaviour import BattlePolicy
 from vgc.datatypes.Objects import GameState
 
 class RandomPolicy(BattlePolicy):
 
+  def __init__(self, player_index=0):
+      self.player_index = player_index
+
   def get_action(self, g: GameState) -> int:
 
-        # Prendo le informazioni sul team: ( pokemon attivo e le sue mosse, pokemon con cui posso fare uno switch )
-        my_team = g.teams[0]
-        my_active = my_team.active
-        my_active_moves = my_active.moves
-        my_switch = my_team.party
-        my_actions = my_active_moves + my_switch
+    # Prendo le informazioni sul team: ( pokemon attivo e le sue mosse, pokemon con cui posso fare uno switch )
+    my_team = g.teams[self.player_index]
+    my_active = my_team.active
+    my_active_moves = my_active.moves
+    my_switch = my_team.party
+    my_actions = my_active_moves + my_switch
 
-        # Do un azione random tra le 4 mosse del pokemon attivo e i due switch con i pokemon in panchina 
-        n_moves = my_active_moves.length 
-        n_switch = my_switch.length
-        n_actions = my_actions.length
-        switch_probability = 0.15   # costante di probabilità di uno switch rispettoa usare una mossa
+    # Do un azione random tra le 4 mosse del pokemon attivo e i due switch con i pokemon in panchina 
+    n_moves = len(my_active_moves) 
+    n_switch = len(my_switch)
+    n_actions = len(my_actions)
+    switch_probability = 0.15   # costante di probabilità di uno switch rispettoa usare una mossa
 
-        # Definizione di una distribuzione di probabilità su tutte le azioni che posso fare
-        pi = ([(1. - switch_probability) / n_moves] * n_moves) + ([switch_probability / n_switch] * n_switch)
+    # Definizione di una distribuzione di probabilità su tutte le azioni che posso fare
+    pi = ([(1. - switch_probability) / n_moves] * n_moves) + ([switch_probability / n_switch] * n_switch)
 
-        # scelta random dell'azione con la distribuzione di probabilità definita prima
-        random_choice = np.random.choice(n_actions, p=pi)
-        return random_choice
+    # scelta random dell'azione con la distribuzione di probabilità definita prima
+    random_choice = np.random.choice(n_actions, p=pi)
+    return random_choice
