39773c4ca302d7161a061c4ca17baf24962a1ef4
mariomoga <142920866+mariomoga@users.noreply.github.com>
2024-12-18 17:41:10 +0100

    Update Final_Combined_Agent.py

--- Agents/Final_Combined_Agent.py
+++ Agents/Final_Combined_Agent.py
@@ -3,6 +3,7 @@ from vgc.datatypes.Constants import TYPE_CHART_MULTIPLIER
 from vgc.datatypes.Objects import GameState, Weather, PkmMove, Pkm, PkmTeam
 from vgc.datatypes.Types import PkmType, WeatherCondition
 from Agents.Logic_Agent import KnowledgeBase
+from copy import deepcopy
 
 import math
 
@@ -54,3 +55,48 @@ class CombinedPolicy(BattlePolicy):
           # START MINIMAX
         else:
           # START MTCS
+
+          # Initializations
+          N = 100
+          state_copy: GameState = deepcopy(state)
+          tree = MonteCarloTreeSearch(
+              player_index=self.player_index,
+              env=state_copy,
+              enable_print=self.enable_print,
+              enable_tree_visualization=self.enable_tree_visualization
+          )
+          # Case of print
+          if self.enable_print:
+              print(f'Simulation phase for player {self.player_index}')
+          # Perform N simulations
+          for i in range(N):
+              if self.enable_print:
+                  print(f'Player: {self.player_index}\nSimulation: {i+1}/{N}')
+              leaf = tree.selection()
+              children = tree.expansion(leaf, number_my_top_moves=2, number_opp_top_moves=3)
+              terminal_nodes = tree.simulation(children)
+              tree.backpropagation(terminal_nodes)
+          # Case of no possible moves
+          if tree.root.children == []:
+              return None
+          # Choose the move
+          best_node = tree.root.children[0]
+          for child in tree.root.children:
+              best_node_utility = best_node.utility_playouts / best_node.total_playouts
+              this_node_utility = child.utility_playouts / child.total_playouts
+              # Case of switch action skipped if there is a difference between the utility values < HEURISTIC_COND1 (~0.02/0.05)
+              if child.actions[self.player_index] > 3 and abs(best_node_utility - this_node_utility) < self.params['HEURISTIC_COND1']:
+                  continue
+              # Case of current node with total number of playouts > HEURISTIC_COND2_1 times the best node's total number of playouts and similar utility values
+              if best_node.total_playouts < child.total_playouts and child.total_playouts > self.params['HEURISTIC_COND2_1'] * best_node.total_playouts \
+                  and abs(this_node_utility - best_node_utility) < self.params['HEURISTIC_COND2_2']:
+                  best_node = child
+                  continue
+              # Case of child with better utility value
+              if this_node_utility > best_node_utility:
+                  best_node = child
+          self.tree = tree
+          # Update the number of switches
+          if best_node.actions[self.player_index] > 3:
+              self.n_switches += 1
+          return best_node.actions[self.player_index]
