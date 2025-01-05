ace6d660d33046f113cf693befd3031228cacea8
Giuseppe Gabriele Russo <khrusso99@hotmail.com>
2025-01-05 10:50:35 +0100

    Added comments to Random agent

--- Agents/Random/Random_Agent.py
+++ Agents/Random/Random_Agent.py
@@ -3,31 +3,48 @@ from vgc.behaviour import BattlePolicy
 from vgc.datatypes.Objects import GameState
 
 class RandomPolicy(BattlePolicy):
+    '''
+    Agent which uses a random policy to choose the next action.
+    '''
 
-  def __init__(self, player_index=0):
-      self.player_index = player_index
-      self.n_switches = 0
+    def __init__(self, player_index=0):
+        '''
+        Initializes the RandomPolicy.
 
-  def get_action(self, g: GameState) -> int:
+        Params:
+        - player_index: the index of the player.
+        '''
+        self.player_index = player_index
+        self.n_switches = 0
 
-    # Prendo le informazioni sul team: ( pokemon attivo e le sue mosse, pokemon con cui posso fare uno switch )
-    my_team = g.teams[self.player_index]
-    my_active = my_team.active
-    my_active_moves = my_active.moves
-    my_switch = my_team.party
-    my_actions = my_active_moves + my_switch
+    def get_action(self, g: GameState) -> int:
+        '''
+        Returns a random action for the current game state.
 
-    # Do un azione random tra le 4 mosse del pokemon attivo e i due switch con i pokemon in panchina 
-    n_moves = len(my_active_moves) 
-    n_switch = len(my_switch)
-    n_actions = len(my_actions)
-    switch_probability = 0.15   # costante di probabilità di uno switch rispettoa usare una mossa
+        Params:
+        - g: the current game state.
 
-    # Definizione di una distribuzione di probabilità su tutte le azioni che posso fare
-    pi = ([(1. - switch_probability) / n_moves] * n_moves) + ([switch_probability / n_switch] * n_switch)
+        Returns:
+        The random action ID.
+        '''
+        # Get information about my team: (active Pokémon and its moves, Pokémon to switch with)
+        my_team = g.teams[self.player_index]
+        my_active = my_team.active
+        my_active_moves = my_active.moves
+        my_switch = my_team.party
+        my_actions = my_active_moves + my_switch
 
-    # scelta random dell'azione con la distribuzione di probabilità definita prima
-    random_choice = np.random.choice(n_actions, p=pi)
-    if random_choice > 3:
-      self.n_switches += 1
-    return random_choice
+        # Choose a random action among the 4 moves of the active Pokémon and the two switches with the Pokémon on the bench
+        n_moves = len(my_active_moves) 
+        n_switch = len(my_switch)
+        n_actions = len(my_actions)
+        switch_probability = 0.15   # constant probability of a switch compared to using a move
+
+        # Define a probability distribution over all possible actions
+        pi = ([(1. - switch_probability) / n_moves] * n_moves) + ([switch_probability / n_switch] * n_switch)
+
+        # Randomly choose an action based on the defined probability distribution
+        random_choice = np.random.choice(n_actions, p=pi)
+        if random_choice > 3:
+            self.n_switches += 1
+        return random_choice
