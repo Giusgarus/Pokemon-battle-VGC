4cc0703223cbf9774398d2cc7b09e1fee01a2c05
Giuseppe Gabriele Russo <khrusso99@hotmail.com>
2025-01-05 10:48:46 +0100

    Added comments to Combined Agent

--- Agents/Combined/Combined_Agent.py
+++ Agents/Combined/Combined_Agent.py
@@ -14,63 +14,83 @@ from vgc.datatypes.Types import PkmStat, PkmType, WeatherCondition
 import math
 
 class CombinedPolicy(BattlePolicy):
+    '''
+    Agent which combines MCTS and MiniMax approaches to choose the next action.
+    '''
 
-  def __init__(self, player_index: int = 0):
-    super().__init__()  # Call the BattlePolicy constructor
-    self.kb = KnowledgeBase()  # KnowledgeBase initialization 
-    self.n_switches = 0
-    self.player_index = player_index
+    def __init__(self, player_index: int = 0):
+        '''
+        Initializes the CombinedPolicy.
 
+        Params:
+        - player_index: the index of the player.
+        '''
+        super().__init__()  # Call the BattlePolicy constructor
+        self.kb = KnowledgeBase()  # KnowledgeBase initialization 
+        self.n_switches = 0
+        self.player_index = player_index
 
-  def set_parameters(self, params: dict):
-    self.params = params
+    def set_parameters(self, params: dict):
+        '''
+        Sets the parameters for the CombinedPolicy.
 
-  def get_action(self, g: GameState) -> int:
+        Params:
+        - params: dictionary containing the parameters.
+        '''
+        self.params = params
 
-    # Get the weather information:
-    weather = g.weather
+    def get_action(self, g: GameState) -> int:
+        '''
+        Returns the best action for the current game state using a combination of MCTS and MiniMax.
 
-    # Get information about my team:
-    my_team = g.teams[self.player_index]
-    my_active = my_team.active
-    my_hp = my_active.hp
-    my_max_hp = my_active.max_hp
-    my_pkm_type = my_active.type
-    my_active_moves = my_active.moves
-    move_types = [my_active_moves[0].type, my_active_moves[1].type, my_active_moves[2].type, my_active_moves[3].type]
-    move_targets = [my_active_moves[0].target, my_active_moves[1].target, my_active_moves[2].target, my_active_moves[3].target]
-    my_switch = my_team.party
-    my_hp_party = [my_switch[0].hp, my_switch[1].hp]
-    my_type_party = [my_switch[0].type, my_switch[1].type]
+        Params:
+        - g: the current game state.
 
-        
-    
-    # Get information about opponent team:
-    opp_team = g.teams[(self.player_index+1)%2]
-    opp_active = opp_team.active
-    opp_pkm_type = opp_active.type
+        Returns:
+        The best action ID.
+        '''
+        # Get the weather information:
+        weather = g.weather
 
-    # update the KB facts:
-    self.kb.update_facts(my_pkm_type, opp_pkm_type, move_types, move_targets, my_hp, my_max_hp, my_hp_party, my_type_party, weather)
+        # Get information about my team:
+        my_team = g.teams[self.player_index]
+        my_active = my_team.active
+        my_hp = my_active.hp
+        my_max_hp = my_active.max_hp
+        my_pkm_type = my_active.type
+        my_active_moves = my_active.moves
+        move_types = [my_active_moves[0].type, my_active_moves[1].type, my_active_moves[2].type, my_active_moves[3].type]
+        move_targets = [my_active_moves[0].target, my_active_moves[1].target, my_active_moves[2].target, my_active_moves[3].target]
+        my_switch = my_team.party
+        my_hp_party = [my_switch[0].hp, my_switch[1].hp]
+        my_type_party = [my_switch[0].type, my_switch[1].type]
 
-    self.kb.clear_actions_priority()
-    self.kb.evaluate()
-    actions_priority = self.kb.get_actions_priority()
-    #take the four best actions
-    top_4_values = sorted(actions_priority, reverse=True)[:4]
-    top_4_indices = [actions_priority.index(value) for value in top_4_values]
+        # Get information about opponent team:
+        opp_team = g.teams[(self.player_index+1)%2]
+        opp_active = opp_team.active
+        opp_pkm_type = opp_active.type
 
-    my_pkms_not_fainted = my_team.get_not_fainted()
-    opp_pkms_not_fainted = opp_team.get_not_fainted()
-    
-    if len(my_pkms_not_fainted) == 0 and len(opp_pkms_not_fainted) == 0:
-      # START MINIMAX
-      top_2_indices = [top_4_indices[0], top_4_indices[1]] 
-      LastPlayer = MiniMaxPlayer(self.player_index, 7)
-      return LastPlayer.get_action(g, top_2_indices)
-    else:
-      # START MTCS
-      MCTSPlayer = MCTSBattlePolicy(self.player_index)
-      MCTSPlayer.set_parameters(self.params)
-      return MCTSPlayer.get_action(g)
-          
+        # Update the KB facts:
+        self.kb.update_facts(my_pkm_type, opp_pkm_type, move_types, move_targets, my_hp, my_max_hp, my_hp_party, my_type_party, weather)
+
+        self.kb.clear_actions_priority()
+        self.kb.evaluate()
+        actions_priority = self.kb.get_actions_priority()
+        # Take the four best actions
+        top_4_values = sorted(actions_priority, reverse=True)[:4]
+        top_4_indices = [actions_priority.index(value) for value in top_4_values]
+
+        my_pkms_not_fainted = my_team.get_not_fainted()
+        opp_pkms_not_fainted = opp_team.get_not_fainted()
+
+        if len(my_pkms_not_fainted) == 0 and len(opp_pkms_not_fainted) == 0:
+            # START MINIMAX
+            top_2_indices = [top_4_indices[0], top_4_indices[1]] 
+            LastPlayer = MiniMaxPlayer(self.player_index, 7)
+            return LastPlayer.get_action(g, top_2_indices)
+        else:
+            # START MCTS
+            MCTSPlayer = MCTSBattlePolicy(self.player_index)
+            MCTSPlayer.set_parameters(self.params)
+            return MCTSPlayer.get_action(g)
+
--- Agents/Combined/Combined_minimax.py
+++ Agents/Combined/Combined_minimax.py
@@ -11,44 +11,55 @@ from vgc.datatypes.Types import PkmStat, PkmType, WeatherCondition
 from vgc.competition.StandardPkmMoves import Struggle
 
 
-from copy import deepcopy
-import numpy as np
-from customtkinter import CTk, CTkButton, CTkRadioButton, CTkLabel
-from typing import Tuple
-
-from vgc.behaviour import BattlePolicy
-from vgc.engine.PkmBattleEnv import PkmBattleEnv
-from vgc.datatypes.Constants import DEFAULT_PKM_N_MOVES, DEFAULT_PARTY_SIZE, TYPE_CHART_MULTIPLIER, DEFAULT_N_ACTIONS
-from vgc.datatypes.Objects import GameState, PkmTeam, PkmMove
-from vgc.datatypes.Types import PkmStat, PkmType, WeatherCondition
-from vgc.competition.StandardPkmMoves import Struggle
-
-
 def damage_prediction(move_type: PkmType, pkm_type: PkmType, move_power: float, opp_pkm_type: PkmType,
                     attack_stage: int, defense_stage: int, weather: WeatherCondition) -> float:
-        stab = 1.5 if move_type == pkm_type else 1.0
-        if (move_type == PkmType.WATER and weather == WeatherCondition.RAIN) or (
-                move_type == PkmType.FIRE and weather == WeatherCondition.SUNNY):
-            weather_modifier = 1.5
-        elif (move_type == PkmType.WATER and weather == WeatherCondition.SUNNY) or (
-                move_type == PkmType.FIRE and weather == WeatherCondition.RAIN):
-            weather_modifier = 0.5
-        else:
-            weather_modifier = 1.0
+    '''
+    Predicts the damage of a move based on various factors.
 
-        stage_level = attack_stage - defense_stage
-        if stage_level >= 0:
-            stage_modifier = (stage_level + 2.0) / 2
-        else:
-            stage_modifier = 2.0 / (abs(stage_level) + 2.0)
+    Params:
+    - move_type: the type of the move.
+    - pkm_type: the type of the Pokémon using the move.
+    - move_power: the power of the move.
+    - opp_pkm_type: the type of the opponent Pokémon.
+    - attack_stage: the attack stage of the Pokémon.
+    - defense_stage: the defense stage of the opponent Pokémon.
+    - weather: the current weather condition.
 
-        damage = TYPE_CHART_MULTIPLIER[move_type][opp_pkm_type] * stab * weather_modifier * stage_modifier * move_power
-        return damage
+    Returns:
+    The predicted damage.
+    '''
+    stab = 1.5 if move_type == pkm_type else 1.0
+    if (move_type == PkmType.WATER and weather == WeatherCondition.RAIN) or (
+            move_type == PkmType.FIRE and weather == WeatherCondition.SUNNY):
+        weather_modifier = 1.5
+    elif (move_type == PkmType.WATER and weather == WeatherCondition.SUNNY) or (
+            move_type == PkmType.FIRE and weather == WeatherCondition.RAIN):
+        weather_modifier = 0.5
+    else:
+        weather_modifier = 1.0
 
+    stage_level = attack_stage - defense_stage
+    if stage_level >= 0:
+        stage_modifier = (stage_level + 2.0) / 2
+    else:
+        stage_modifier = 2.0 / (abs(stage_level) + 2.0)
 
+    damage = TYPE_CHART_MULTIPLIER[move_type][opp_pkm_type] * stab * weather_modifier * stage_modifier * move_power
+    return damage
 
 
 def simulate_move(game: GameState, move_id: int, player: bool) -> GameState:
+    '''
+    Simulates a move and returns the new game state.
+
+    Params:
+    - game: the current game state.
+    - move_id: the ID of the move to simulate.
+    - player: the player making the move (0 or 1).
+
+    Returns:
+    The new game state after the move.
+    '''
     new_g = deepcopy(game)
 
     if player != 0 and player != 1:
@@ -80,9 +91,17 @@ def simulate_move(game: GameState, move_id: int, player: bool) -> GameState:
     return new_g
 
 
-
-
 def get_residual_hp(game: GameState, player: bool) -> Tuple[int, int]:
+    '''
+    Calculates the total residual HP for both teams.
+
+    Params:
+    - game: the current game state.
+    - player: the player index (0 or 1).
+
+    Returns:
+    A tuple containing the total residual HP for both teams.
+    '''
     my_team = game.teams[0]
     opp_team = game.teams[1]
     my_tot_hp=0
@@ -95,17 +114,32 @@ def get_residual_hp(game: GameState, player: bool) -> Tuple[int, int]:
     return my_tot_hp, opp_tot_hp
 
 
-
-
 class MiniMaxBattlePolicy:
     
     def __init__(self, depth:int, player: bool, life_value: int = 500):
+        '''
+        Initializes the MiniMaxBattlePolicy.
+
+        Params:
+        - depth: the depth of the minimax search.
+        - player: the player index (0 or 1).
+        - life_value: the value of life points.
+        '''
         self.player = player
         self.depth = depth
         self.life_value = life_value
 
     def evaluate(self, game: GameState, player: bool) -> float:
+        '''
+        Evaluates the game state and returns a score.
 
+        Params:
+        - game: the current game state.
+        - player: the player index (0 or 1).
+
+        Returns:
+        The evaluation score.
+        '''
         if player != 0 and player != 1:
             raise ValueError('Player must be 0 or 1')
 
@@ -117,7 +151,7 @@ class MiniMaxBattlePolicy:
         my_score = 0
         opp_score = 0
 
-        # Considera le win condition
+        # Consider win conditions
         my_tot_hp, opp_tot_hp = get_residual_hp(game, player)
 
         if(opp_tot_hp == 0):
@@ -125,18 +159,17 @@ class MiniMaxBattlePolicy:
         elif(my_tot_hp == 0):
             opp_score += 2000
 
-
-        # Considera la salute residua dei Pokémon
+        # Consider residual health of Pokémon
         for pkm in my_team.get_pkm_list():
             my_score += self.life_value*(pkm.hp / pkm.max_hp)
         for pkm in opp_team.get_pkm_list():
             opp_score += self.life_value*(pkm.hp / pkm.max_hp)
 
-        # Considera il numero di Pokémon rimasti
+        # Consider the number of remaining Pokémon
         my_score += 500*len([pkm for pkm in my_team.get_pkm_list() if pkm.hp > 0])
         opp_score += 500*len([pkm for pkm in opp_team.get_pkm_list() if pkm.hp > 0])
 
-        # Considera le condizioni di stato
+        # Consider status conditions
         for pkm in my_team.get_pkm_list():
             if pkm.status == 1:
                 my_score -= 100
@@ -164,8 +197,7 @@ class MiniMaxBattlePolicy:
             elif pkm.status == 6:
                 opp_score -= 100
 
-
-        # Considera il vantaggio di tipo
+        # Consider type advantage
         my_active_pkm = my_team.active
         opp_active_pkm = opp_team.active
         if my_active_pkm and opp_active_pkm:
@@ -179,38 +211,36 @@ class MiniMaxBattlePolicy:
             elif TYPE_CHART_MULTIPLIER[opp_active_pkm.type][my_active_pkm.type] == .5:
                 opp_score -= 100
 
-
-
-        # Considera il vantaggio delle mosse per condizioni meteorologiche del team
+        # Consider move advantage based on weather conditions for the team
         weather = game.weather
         for i,move in enumerate(my_team.active.moves):
             move_type = move.type
             if weather.condition == 1: # SUNNY
-                if move_type == 1: # mossa fuoco
+                if move_type == 1: # fire move
                     my_score += 150
-                elif move_type == 2: # mossa acqua 
+                elif move_type == 2: # water move
                     my_score -= 150
             elif weather.condition == 2: # RAIN
-                if move_type == 2: # mossa fuoco
+                if move_type == 2: # fire move
                     my_score += 150
-                elif move_type == 1: # mossa acqua 
+                elif move_type == 1: # water move
                     my_score -= 150
-        # Considera il vantaggio delle mosse per condizioni meteorologiche del team avversario
+        # Consider move advantage based on weather conditions for the opponent team
         for i,move in enumerate(opp_team.active.moves):
             move_type = move.type
             if weather.condition == 1: # SUNNY
-                if move_type == 1: # mossa fuoco
+                if move_type == 1: # fire move
                     opp_score += 150
-                elif move_type == 2: # mossa acqua 
+                elif move_type == 2: # water move
                     opp_score -= 150
             elif weather.condition == 2: # RAIN
-                if move_type == 2: # mossa fuoco
+                if move_type == 2: # fire move
                     opp_score += 150
-                elif move_type == 1: # mossa acqua 
+                elif move_type == 1: # water move
                     opp_score -= 150
-        #Considera il vantaggio di tipo per le condizioni meteorologiche 
+        # Consider type advantage based on weather conditions
         if weather.condition == 3: # SANDSTORM
-            if my_team.active.type == 12: # Pokemon roccia
+            if my_team.active.type == 12: # rock Pokémon
                 my_score += 150
             else:
                 my_score -= 150
@@ -219,7 +249,7 @@ class MiniMaxBattlePolicy:
             else:
                 opp_score -= 150
         elif weather.condition == 4: # HAIL
-            if my_team.active.type == 5: # Pokemon roccia
+            if my_team.active.type == 5: # ice Pokémon
                 my_score += 150
             else:
                 my_score -= 150
@@ -227,13 +257,22 @@ class MiniMaxBattlePolicy:
                 opp_score += 150
             else:
                 opp_score -= 150
-                    
 
         return my_score - opp_score
 
-
-    
     def mini(self, game: GameState, depth: int, alpha, beta) -> float:
+        '''
+        Implements the minimization step of the minimax algorithm.
+
+        Params:
+        - game: the current game state.
+        - depth: the current depth of the search.
+        - alpha: the alpha value for alpha-beta pruning.
+        - beta: the beta value for alpha-beta pruning.
+
+        Returns:
+        The minimum evaluation score.
+        '''
         residual_hp = get_residual_hp(game, 0)
         if depth == 0 or residual_hp[1] ==0 or residual_hp[0] == 0:
             return self.evaluate(game, self.player)
@@ -245,13 +284,24 @@ class MiniMaxBattlePolicy:
                 eval = self.max(new_game_state, depth - 1, alpha, beta)
                 min_eval = min(min_eval, eval)
                 beta = min(beta, eval)
-                #CONTROLLA CONDIZIONE
+                # Check condition
                 if eval <= alpha:
                     break
             return min_eval + self.evaluate(game, self.player)
 
-
     def max(self, game: GameState, depth: int, alpha, beta) -> float:
+        '''
+        Implements the maximization step of the minimax algorithm.
+
+        Params:
+        - game: the current game state.
+        - depth: the current depth of the search.
+        - alpha: the alpha value for alpha-beta pruning.
+        - beta: the beta value for alpha-beta pruning.
+
+        Returns:
+        The maximum evaluation score.
+        '''
         residual_hp = get_residual_hp(game, 0)
         if depth == 0 or residual_hp[1] ==0 or residual_hp[0] == 0:
             return self.evaluate(game, self.player)
@@ -263,14 +313,22 @@ class MiniMaxBattlePolicy:
                 eval = self.mini(new_game_state, depth - 1, alpha, beta)
                 max_eval = max(max_eval, eval)
                 alpha = max(alpha, eval)
-                #CONTROLLA CONDIZIONE
+                # Check condition
                 if beta <= eval:
                     break
             return max_eval +self.evaluate(game, self.player)
 
-
-    
     def minimax(self, game: GameState, top2moves: list) -> int:
+        '''
+        Implements the minimax algorithm to find the best move.
+
+        Params:
+        - game: the current game state.
+        - top2moves: list of top 2 moves to consider.
+
+        Returns:
+        The best move ID.
+        '''
         best_move = None
         best_value = float('-inf')
         idx = 0 if self.player == 0 else 1
@@ -283,17 +341,32 @@ class MiniMaxBattlePolicy:
         return best_move
 
 
-
 class MiniMaxPlayer(BattlePolicy):
 
     def __init__(self, player_index = 0, depth: int = 5):
+        '''
+        Initializes the MiniMaxPlayer.
+
+        Params:
+        - player_index: the index of the player.
+        - depth: the depth of the minimax search.
+        '''
         self.player_index = player_index
         self.depth = depth
         self.n_switches = 0
         super().__init__()
-        
 
     def get_action(self, game: GameState,  top2moves: list) -> int:
+        '''
+        Returns the best action for the current game state.
+
+        Params:
+        - game: the current game state.
+        - top2moves: list of top 2 moves to consider.
+
+        Returns:
+        The best action ID.
+        '''
         policy = MiniMaxBattlePolicy(self.depth,self.player_index, 300)
         best_move = policy.minimax(game, top2moves)
         if best_move in [4,5]:
